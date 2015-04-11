fc88d629e009256736fdb2d74a7415610198c87d
Alexey Gladkov <gladkov.alexey@gmail.com>
2015-04-12 01:08:32 +0300

    Add ability to load kernel modules
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- guest/init.c
+++ guest/init.c
@@ -3,15 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <errno.h>
 #include <error.h>
 
+#include <sys/param.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/mount.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 #include <sys/reboot.h>
 
@@ -26,17 +30,20 @@
 			sigaction(sig, &sa, NULL); \
 		} while(0)
 
+#define MODULES_FILE "/virt/etc/modules"
 #define SANDBOX_EXEC "/virt/sandbox.sh"
 #define SHELL_EXEC   "/bin/sh"
 
-static const char *sandbox_prog[] = { SANDBOX_EXEC, (char *) 0 };
-static const char *shell_prog[]   = { SHELL_EXEC,   (char *) 0 };
+static const char *sandbox_prog[] = { SANDBOX_EXEC, NULL };
+static const char *shell_prog[]   = { SHELL_EXEC,   NULL };
 
 static const char *const run_env[] = {
 	"TERM=linux",
 	"HOME=/virt/home",
 	NULL
 };
+
+extern long init_module(void *, unsigned long, const char *);
 
 static sig_atomic_t got_ECHILD;
 
@@ -157,6 +164,108 @@ sysreboot(void)
 	for (;;) pause();
 }
 
+static const char *
+moderror(int err)
+{
+	switch (err) {
+		case ENOEXEC:
+			return "Invalid module format";
+		case ENOENT:
+			return "Unknown symbol in module";
+		case ESRCH:
+			return "Module has wrong symbol version";
+		case EINVAL:
+			return "Invalid parameters";
+	}
+	return strerror(err);
+}
+
+static void *
+mmap_module(const char *filename, size_t *image_size_p)
+{
+	void *image = NULL;
+	struct stat st;
+	size_t image_size;
+	int fd;
+
+	if ((fd = open(filename, O_RDONLY)) == -1) {
+		perror("open");
+		return NULL;
+	}
+
+	if (fstat(fd, &st) == -1) {
+		perror("fstat");
+		close(fd);
+		return NULL;
+	}
+
+	image_size = st.st_size;
+
+	if ((image = mmap(NULL, image_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+		perror("mmap");
+		close(fd);
+		return NULL;
+	}
+
+	*image_size_p = image_size;
+
+	close(fd);
+	return image;
+}
+
+static int
+load_modules(void)
+{
+	char path[MAXPATHLEN];
+
+	FILE *f = NULL;
+	int rc = 0;
+
+	size_t image_size;
+	void *image;
+	const char image_params[] = "";
+
+	if (access(MODULES_FILE, R_OK) != 0) {
+		printf("%s not found. Nothing to do ...\n", MODULES_FILE);
+		return 0;
+	}
+
+	if ((f = fopen(MODULES_FILE, "r")) == NULL) {
+		perror("fopen");
+		return 1;
+	}
+
+	while (fscanf(f, "%s", path) != EOF) {
+		image_size = 0;
+
+		image = mmap_module(path, &image_size);
+		if (!image) {
+			continue;
+		}
+
+		if (init_module(image, image_size, image_params) != 0) {
+			if (errno != EEXIST) {
+				fprintf(stderr, "Error: init_module: %s: %s\n", path, moderror(errno));
+
+				munmap(image, image_size);
+
+				rc = 1;
+				break;
+			}
+		}
+
+		munmap(image, image_size);
+	}
+
+	if (ferror(f)) {
+		perror("fscanf");
+		rc = 1;
+	}
+
+	fclose(f);
+	return rc;
+}
+
 int
 main(void)
 {
@@ -167,6 +276,9 @@ main(void)
 	SETSIG(sa, SIGTSTP, SIG_IGN,      SA_RESTART);
 	SETSIG(sa, SIGQUIT, SIG_IGN,      SA_RESTART);
 	SETSIG(sa, SIGCHLD, chld_handler, SA_RESTART);
+
+	if (load_modules())
+		return 1;
 
 	do_mounts();
 
